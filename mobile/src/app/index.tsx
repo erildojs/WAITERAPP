345738e0a4e4b774595d7fed958d66b0f4c403db
ErildoJS <erildofranciscojs@gmail.com>
2025-01-28 15:17:51 +0100

    configured fonts

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,7 +1,13 @@
 import { View } from "react-native";
+import { useFonts } from 'expo-font'
 
 export default function Index() {
-
+  const [isFontsLoaded] = useFonts({
+    GeneralSans400: require('../assets/fonts/GeneralSans-Regular.otf'),
+    GeneralSans500: require('../assets/fonts/GeneralSans-Semibold.otf'),
+    GeneralSans600: require('../assets/fonts/GeneralSans-Bold.otf'),
+  })
+  if (!isFontsLoaded) return null
   return (
     <View></View>
   );
