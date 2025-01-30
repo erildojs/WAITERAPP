9600a3f5afb929399d0a47487db9ee3a646b3148
ErildoJS <erildofranciscojs@gmail.com>
2025-01-30 11:44:27 +0100

    feat: add header component

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,14 +1,14 @@
-import { View } from "react-native";
 import { useFonts } from 'expo-font'
+import { Main } from "./Main";
 
 export default function Index() {
   const [isFontsLoaded] = useFonts({
     GeneralSans400: require('../assets/fonts/GeneralSans-Regular.otf'),
-    GeneralSans500: require('../assets/fonts/GeneralSans-Semibold.otf'),
-    GeneralSans600: require('../assets/fonts/GeneralSans-Bold.otf'),
+    GeneralSans600: require('../assets/fonts/GeneralSans-Semibold.otf'),
+    GeneralSans700: require('../assets/fonts/GeneralSans-Bold.otf'),
   })
   if (!isFontsLoaded) return null
   return (
-    <View></View>
+    <Main />
   );
 }
