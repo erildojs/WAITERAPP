be415acb7cad05d9f28c4bf7174228e1d889a9b2
ErildoJS <erildofranciscojs@gmail.com>
2025-01-31 21:56:09 +0100

    feat: add catgories component

--- mobile/src/app/Main/index.tsx
+++ mobile/src/app/Main/index.tsx
@@ -1,10 +1,24 @@
-import { Container } from "./styles";
+import React from "react";
+import { CategoriesContainer, Container, Footer, FooterContainer, MenuContainer } from "./styles";
 import { Header } from "../components/Header";
+import { Categories } from "../components/Categories";
+import { Menu } from "../components/Menu";
 
 export function Main() {
   return (
-    <Container>
-      <Header />
-    </Container>
+    <>
+      <Container>
+        <Header />
+        <CategoriesContainer>
+          <Categories />
+        </CategoriesContainer>
+        <MenuContainer>
+          <Menu />
+        </MenuContainer>
+      </Container>
+      <Footer>
+        <FooterContainer></FooterContainer>
+      </Footer>
+    </>
   )
 }
---
+++ mobile/src/app/components/Categories/index.tsx
@@ -0,0 +1,33 @@
+import { categories } from "@/app/mocks/categories";
+import { Text } from "../Text";
+import { Category, Icon } from "./styles";
+import { FlatList } from "react-native";
+import { useState } from "react";
+
+export function Categories() {
+  const [selectedCategory, setSelectedCategory] = useState<string | null>(null)
+  function handleSelectCategory(categoryId: string) {
+    const category = selectedCategory === categoryId ? null : categoryId
+    setSelectedCategory(categoryId)
+  }
+
+  return (
+    <FlatList horizontal
+      data={categories}
+      showsHorizontalScrollIndicator={false}
+      contentContainerStyle={{ paddingRight: 24 }}
+      keyExtractor={category => category._id}
+      renderItem={({ item: category }) => {
+        const isSelected = selectedCategory === category._id
+        return (
+          <Category onPress={() => handleSelectCategory(category._id)}>
+            <Icon>
+              <Text opacity={isSelected ? 1 : 0.5}>{category.icon}</Text>
+            </Icon>
+            <Text size={14} weight="600">{category.name}</Text>
+          </Category>
+        )
+      }}
+    />
+  )
+}
