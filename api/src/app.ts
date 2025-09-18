bcea1a3ca640b762059d23ceecb13202ae9c5449
ErildoJS <erildofranciscojs@gmail.com>
2025-09-18 11:19:50 +0100

    fix: remove some log

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,6 +1,6 @@
 import { app } from "./server";
 
 app.listen(process.env.API_PORT, () => {
-  console.log('server is running on por', + process.env.API_PORT);
+  console.log('server is running on por', process.env.API_PORT);
   console.log('Documentação no endereço:', `${process.env.API_PORT}`, '/docs');
 })
